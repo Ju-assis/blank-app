f237022a616705a500f912d3e5e050fc1b633ae6
Juliana de Assis <baptistadeassis@gmail.com>
2024-08-22 11:12:40 -0400

    estrutura_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
-import streamlit as st
+iimport streamlit as st
+import pandas as pd
+import altair as alt
+from PIL import Image
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+@st.cache_data
+def gerar_df():
+  df=pd.read_excel(
+    io="estrutura_tarifaria_2024.xlsx",
+    engine="openpyxl",
+    sheet_name="Table 1",
+    usecols="A:I",
+    nrows=28
+  )
+  return df
+df=gerar_df()
+with st.sidebar:
+  logo=Image.open('logo_sanesul.png')
+  st.image(logo, use_column_width=True)
+  
+def click_button ():
+  print("teste")
+  st.write(valor)
+  
+st.title("Cálculo consumo de água")
+st.header("Estrutura tarifária Sanesul")
+
+valor = st.text_input("Informe seu consumo de água")
+st.button('Calcular', on_click=click_button)
